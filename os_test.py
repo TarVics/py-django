900debea7289d1f3277202a20eca31a269260386
Victor Taran <tarvics@gmail.com>
2023-03-15 22:36:39 +0200

    os test

---
+++ os_test.py
@@ -0,0 +1,22 @@
+import os
+
+# print(os.uname())
+# print(os.environ)
+# os.system('htop')
+
+print(os.getcwd())
+# os.mkdir('first')
+# os.mkdir('second')
+
+# c:\folder1\folder2\
+#     /folder1/folder2
+
+# path = os.path.join('folder1', 'folder2', '111.txt')
+#
+# print(path)
+
+# for root, folders, files in os.walk('.'):
+#     print(f'{root=} {folders=} {files=}')
+
+# os.rename('os_test.py', 'os_test2.py')
+# os.rename('os_test2.py', os.path.join('first', 'os_test.py'))
